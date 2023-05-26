4a7f69de762e82c103320fb05a51b0470b3ba305
mohitsonu <mohitsonu33@gmail.com>
2023-05-26 20:04:30 +0530

    test1

--- app.js
+++ app.js
@@ -1,64 +1,76 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 const app = express();
-
-var arrayitem = [];
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-    var today = new Date();
-    var options = {
-        day: "numeric",
-        weekday: "long",
-        month: "long",
-        year: "numeric",
-    };
-    var day = today.toLocaleDateString("en-in", options);
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-    res.render("list", { daytype: day, newlistitems: arrayitem, error: null });
+app.get("/", function (req, res) {
+  var today = new Date();
+  var options = {
+    day: "numeric",
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+  };
+  var day = today.toLocaleDateString("en-in", options);
+
+  if (!req.session.taskList) {
+    req.session.taskList = [];
+  }
+
+  res.render("list", { daytype: day, newlistitems: req.session.taskList });
 });
 
 app.post("/add-task", function (req, res) {
-    var item = req.body.newitems;
-    if (item.trim() === "") {
-        var today = new Date();
-        var options = {
-            day: "numeric",
-            weekday: "long",
-            month: "long",
-            year: "numeric",
-        };
-        var day = today.toLocaleDateString("en-in", options);
+  var item = req.body.newitems;
 
-        res.render("list", {
-            daytype: day,
-            newlistitems: arrayitem,
-            error: "Please! Add a task",
-        });
-    } else {
-        arrayitem.push(item);
-        res.redirect("/");
-    }
+  if (item.trim() === "") {
+    // Handle empty task error
+    var today = new Date();
+    var options = {
+      day: "numeric",
+      weekday: "long",
+      month: "long",
+      year: "numeric",
+    };
+    var day = today.toLocaleDateString("en-in", options);
+
+    res.render("list", {
+      daytype: day,
+      newlistitems: req.session.taskList,
+      error: "Task cannot be empty",
+    });
+  } else {
+    req.session.taskList.push(item);
+    res.redirect("/");
+  }
 });
 
 app.post("/delete-task", function (req, res) {
-    let index = 0;
-    let newArr = [];
-    for (var i = 0; i < arrayitem.length; i++) {
-        if (arrayitem[i] == req.body.task) {
-            continue;
-        }
-        newArr.push(arrayitem[i]);
-    }
-    arrayitem = newArr;
-    res.redirect("/");
+  let task = req.body.task;
+  let taskList = req.session.taskList;
+
+  let index = taskList.indexOf(task);
+  if (index > -1) {
+    taskList.splice(index, 1);
+  }
+
+  res.redirect("/");
 });
 
 app.listen(4000, function () {
-    console.log("server started at port 4000");
+  console.log("Server started at port 4000");
 });
