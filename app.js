c8af2f30d94bcec90193fb813b1c8f9e062f730b
mohitsonu <mohitsonu33@gmail.com>
2023-05-26 23:14:32 +0530

    final build 1.3

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const crypto = require("crypto");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    secret: generateRandomKey(),
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -27,47 +17,47 @@ app.get("/", function (req, res) {
   };
   var day = today.toLocaleDateString("en-in", options);
 
-  var arrayitem = req.session.arrayitem || [];
-
-  res.render("list", { daytype: day, newlistitems: arrayitem, error: null });
+    res.render("list", { daytype: day, newlistitems: arrayitem, error: null });
 });
 
 app.post("/add-task", function (req, res) {
-  var item = req.body.newitems;
+    var item = req.body.newitems;
+    if (item.trim() === "") {
+        var today = new Date();
+        var options = {
+            day: "numeric",
+            weekday: "long",
+            month: "long",
+            year: "numeric",
+        };
+        var day = today.toLocaleDateString("en-in", options);
 
-  if (item.trim() === "") {
-    var today = new Date();
-    var options = {
-      day: "numeric",
-      weekday: "long",
-      month: "long",
-      year: "numeric",
-    };
-    var day = today.toLocaleDateString("en-in", options);
-
-    res.render("list", {
-      daytype: day,
-      newlistitems: req.session.arrayitem || [],
-      error: "Task cannot be empty",
-    });
-  } else {
-    req.session.arrayitem = req.session.arrayitem || [];
-    req.session.arrayitem.push(item);
-
-    res.redirect("/");
-  }
+        res.render("list", {
+            daytype: day,
+            newlistitems: arrayitem,
+            error: "Please! Add a task",
+        });
+    } else {
+        arrayitem.push(item);
+        res.redirect("/");
+    }
 });
 
 app.post("/delete-task", function (req, res) {
-  var task = req.body.task;
-
-  req.session.arrayitem = req.session.arrayitem.filter((item) => item !== task);
-
-  res.redirect("/");
+    let index = 0;
+    let newArr = [];
+    for (var i = 0; i < arrayitem.length; i++) {
+        if (arrayitem[i] == req.body.task) {
+            continue;
+        }
+        newArr.push(arrayitem[i]);
+    }
+    arrayitem = newArr;
+    res.redirect("/");
 });
 
 app.listen(4000, function () {
-  console.log("server started at port 4000");
+    console.log("server started at port 4000");
 });
 
 // Function to generate a random secret key
