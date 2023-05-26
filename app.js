ce7e8707e116aee1fafe7e0d2183da2cebdee2fe
mohitsonu <mohitsonu33@gmail.com>
2023-05-26 15:51:36 +0530

    final build 1

--- app.js
+++ app.js
@@ -8,37 +8,48 @@ var arrayitem = [];
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-``
 app.get("/", function (req, res) {
-
     var today = new Date();
-
     var options = {
-
         day: "numeric",
         weekday: "long",
         month: "long",
         year: "numeric",
     };
-
     var day = today.toLocaleDateString("en-in", options);
 
-    res.render("list", { daytype: day, newlistitems: arrayitem});
+    res.render("list", { daytype: day, newlistitems: arrayitem, error: null });
 });
 
-app.post("/", function (req, res) 
-{
+app.post("/add-task", function (req, res) {
     var item = req.body.newitems;
-    arrayitem.push(item);
-    res.redirect("/");
+    if (item.trim() === "") {
+        var today = new Date();
+        var options = {
+            day: "numeric",
+            weekday: "long",
+            month: "long",
+            year: "numeric",
+        };
+        var day = today.toLocaleDateString("en-in", options);
+
+        res.render("list", {
+            daytype: day,
+            newlistitems: arrayitem,
+            error: "Please! Add a task",
+        });
+    } else {
+        arrayitem.push(item);
+        res.redirect("/");
+    }
 });
 
-app.post("/delete-task", function(req, res) {
+app.post("/delete-task", function (req, res) {
     let index = 0;
     let newArr = [];
-    for(var i = 0; i < arrayitem.length; i++) {
+    for (var i = 0; i < arrayitem.length; i++) {
         if (arrayitem[i] == req.body.task) {
             continue;
         }
@@ -46,7 +57,7 @@ app.post("/delete-task", function(req, res) {
     }
     arrayitem = newArr;
     res.redirect("/");
-})
+});
 
 app.listen(4000, function () {
     console.log("server started at port 4000");
